739d2982432e2c3d7914bd610548702325e8b78e
chris <christopherjosephdobbins@gmail.com>
2019-03-31 10:27:43 -0400

    more ugly rudimentary unit tests

--- game/game_test.go
+++ game/game_test.go
@@ -1,103 +1,127 @@
 package game
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestUpdate(t *testing.T) {
 	g := Setup("hello", 6)
 	// testing field that's displayed to user
-	// scenario: incorrect guess
+	// scenario: valid but incorrect guess
 	g.Update('a')
-	expected := []string{"_", "_", "_", "_", "_", "_"}
-        expectedGuessedChars := map[rune]struct{}{'a': struct{}{}}
-        if len(expectedGuessedChars) != len(g.guessedChars) {
-          t.Logf("expected %d guessedChars in g.guessedChars, actual number was %d", len(expectedGuessedChars), len(g.guessedChars))
-          t.Fail()
-        }
-        missedChars := []rune{}
-        for ch, _ := range g.guessedChars {
-             if _, ok := expectedGuessedChars[ch]; !ok {
-                 t.Logf("expected %c not found in g.guessedChars", ch)
-                 missedChars = append(missedChars, ch)
-             }
-        }
-        if len(missedChars) > 0 {
-            t.Fail()
-        }
-	failedIndices := []int{}
-	for idx, ch := range g.disp {
-		if ch != expected[idx] {
-			failedIndices = append(failedIndices, idx)
-			t.Logf("expected %s at index %d, got %v", expected[idx], idx, ch)
-		}
-	}
-	if len(failedIndices) > 0 {
+	// test `g.guessedChars` field
+	expectedGuessedChars := map[rune]struct{}{'a': struct{}{}}
+	if len(expectedGuessedChars) != len(g.guessedChars) {
+		t.Logf("expected %d guessedChars in g.guessedChars, actual number was %d", len(expectedGuessedChars), len(g.guessedChars))
 		t.Fail()
 	}
+	missedChars := []rune{}
+	for ch, _ := range g.guessedChars {
+		if _, ok := expectedGuessedChars[ch]; !ok {
+			t.Logf("expected %c not found in g.guessedChars", ch)
+			missedChars = append(missedChars, ch)
+		}
+	}
+	if len(missedChars) > 0 {
+		t.Fail()
+	}
+	// test g.disp
+	expectedDisplay := []string{"_", "_", "_", "_", "_"}
+	if strings.Join(expectedDisplay, "") != strings.Join(g.disp, "") {
+		t.Logf("expected `g.disp` to be: %v, got %v", expectedDisplay, g.disp)
+		t.Fail()
+	}
+	// END incorrect, but valid test
 
+	// scenario: correct lowercase guess
 	g = Setup("world", 6)
-	// correct guess
 	g.Update('o')
-	expected = []string{"_", "o", "_", "_", "_"}
-	for idx, ch := range g.disp {
-		if ch != expected[idx] {
-			failedIndices = append(failedIndices, idx)
-			t.Logf("expected %s at index %d, got %v", expected[idx], idx, ch)
+	// test g.guessedChars
+	missedChars = []rune{}
+	expectedGuessedChars = map[rune]struct{}{'o': struct{}{}}
+	for ch, _ := range g.guessedChars {
+		if _, ok := expectedGuessedChars[ch]; !ok {
+			t.Logf("expected %c not found in g.guessedChars", ch)
+			missedChars = append(missedChars, ch)
 		}
 	}
-	if len(failedIndices) > 0 {
+	if len(missedChars) > 0 {
 		t.Fail()
 	}
+	// test g.disp
+	expectedDisplay = []string{"_", "o", "_", "_", "_"}
+	if strings.Join(expectedDisplay, "") != strings.Join(g.disp, "") {
+		t.Logf("expected `g.disp` to be: %v, got %v", expectedDisplay, g.disp)
+		t.Fail()
+	}
+	// end correct lowercase guess
 
-	// correct guess that appears multiple times in word
+	// scenario: correct lowercase guess that appears multiple times in word
 	g = Setup("arrrgh", 6)
 	g.Update('r')
-	expected = []string{"_", "r", "r", "r", "_", "_"}
-	for idx, ch := range g.disp {
-		if ch != expected[idx] {
-			failedIndices = append(failedIndices, idx)
-			t.Logf("expected %s at index %d, got %v", expected[idx], idx, ch)
+	// test `g.guessedChars` field
+	missedChars = []rune{}
+	expectedGuessedChars = map[rune]struct{}{'r': struct{}{}}
+	for ch, _ := range g.guessedChars {
+		if _, ok := expectedGuessedChars[ch]; !ok {
+			t.Logf("expected %c not found in g.guessedChars", ch)
+			missedChars = append(missedChars, ch)
 		}
 	}
-	if len(failedIndices) > 0 {
+	if len(missedChars) > 0 {
 		t.Fail()
 	}
+	// test `g.expectedDisplay`
+	expectedDisplay = []string{"_", "r", "r", "r", "_", "_"}
+	if strings.Join(expectedDisplay, "") != strings.Join(g.disp, "") {
+		t.Logf("expected `g.disp` to be: %v, got %v", expectedDisplay, g.disp)
+		t.Fail()
+	}
+	// end correct lowercase guess
 
+	// scenario: upper case but otherwise correct guess
 	g = Setup("hello", 6)
-	// upper case but otherwise correct guess
 	g.Update('L')
-	expected = []string{"_", "_", "l", "l", "_"}
-	for idx, ch := range g.disp {
-		if ch != expected[idx] {
-			failedIndices = append(failedIndices, idx)
-			t.Logf("expected %s at index %d, got %v", expected[idx], idx, ch)
+	// test `g.guessedChars`
+	expectedGuessedChars = map[rune]struct{}{'l': struct{}{}}
+	missedChars = []rune{}
+	for ch, _ := range g.guessedChars {
+		if _, ok := expectedGuessedChars[ch]; !ok {
+			t.Logf("expected %c not found in g.guessedChars", ch)
+			missedChars = append(missedChars, ch)
 		}
 	}
-	if len(failedIndices) > 0 {
+	if len(missedChars) > 0 {
 		t.Fail()
 	}
+	// test `g.disp`
+	expectedDisplay = []string{"_", "_", "l", "l", "_"}
+	if strings.Join(expectedDisplay, "") != strings.Join(g.disp, "") {
+		t.Logf("expected `g.disp` to be: %v, got %v", expectedDisplay, g.disp)
+		t.Fail()
+	}
+	// end test uppercase correct guess
 
+	// scenario: invalid character guess
 	g = Setup("hello", 7)
-	// invalid character
 	g.Update('#')
-	expected = []string{"_", "_", "_", "_", "_"}
-
-	for idx, ch := range g.disp {
-		if ch != expected[idx] {
-			failedIndices = append(failedIndices, idx)
-			t.Logf("expected %s at index %d, got %v", expected[idx], idx, ch)
-		}
-	}
-	if len(failedIndices) > 0 {
+	expectedDisplay = []string{"_", "_", "_", "_", "_"}
+	if strings.Join(expectedDisplay, "") != strings.Join(g.disp, "") {
+		t.Logf("expected `g.disp` to be: %v, got %v", expectedDisplay, g.disp)
 		t.Fail()
 	}
+	// test g.warning
 	if g.warning != "Invalid input. Please enter a letter." {
 		t.Logf("expected g.warning to be %s, actual value is %v", "Invalid input. Please enter a letter.", g.warning)
-                t.Fail()
+		t.Fail()
 	}
+	// test g.remainingGuesses
 	if g.remainingGuesses != 7 {
 		t.Logf("expected g.remainingGuesses to be %d, actual value is %v", 7, g.remainingGuesses)
 		t.Fail()
 	}
+	// test g.guessedChars
 	expectedGuessedChars = map[rune]struct{}{}
 	if len(g.guessedChars) > 0 {
 		t.Logf("expected g.guessedChars to be %v, actual is %v", expectedGuessedChars, g.guessedChars)
