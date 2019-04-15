c7de9c6d6ea8773151fdeaa33dfb87a89599c0f9
chris <christopherjosephdobbins@gmail.com>
2019-04-15 12:35:10 -0400

    putting display dep installation in one place

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -3,8 +3,8 @@
 user=$(id -u)
 
 if [ $user -ne 0 ]; then 
-echo "must be root to run this" 
-exit 1
+        echo "must be root to run this" 
+        exit 1
 fi
 
 echo "starting install..."
@@ -14,16 +14,6 @@ apt-get update -y
 if [ $? -ne 0 ]; then
        echo "failed to update OS"
        exit 1
-fi
-
-hash git 2> /dev/null
-if [ $? -ne 0 ]; then
-       echo "installing git"
-       apt-get install -y git
-       if [ $? -ne 0 ]; then
-               echo "failed to install git"
-               exit 1
-       fi
 fi
 
 echo "installing game"
@@ -36,7 +26,18 @@ mv game-linux-arm /usr/local/bin
 chmod +x /usr/local/bin/game-linux-arm
 echo "game installed successfully! to start, type game-linux-arm"
 
+
 echo "downloading configuration for 3.5\" display"
+hash git 2> /dev/null
+if [ $? -ne 0 ]; then
+       echo "installing git"
+       apt-get install -y git
+       if [ $? -ne 0 ]; then
+               echo "failed to install git"
+               exit 1
+       fi
+fi
+
 git clone https://github.com/goodtft/LCD-show
 cd LCD-show
 # modify install script to continue instead of waiting for user approval
